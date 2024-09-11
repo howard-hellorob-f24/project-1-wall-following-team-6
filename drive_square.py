213ad1b890353e7814fa974f9959e20a7c902af2
v <violetedwardsjane@gmail.com>
2024-09-11 14:08:21 -0400

    Add drive square

--- drive_square.py
+++ drive_square.py
@@ -3,7 +3,21 @@ from mbot_bridge.api import MBot
 
 robot = MBot()
 
+def drive_square():
+    for i in range(4):
+        robot.drive(1.0,0,0)
+        time.sleep(1)
+        robot.drive(0,0,1.57)
+        time.sleep(1)
+
+def drive_square_three_times():
+    for i in range(3):
+        drive_square()
+
+drive_square_three_times()
+
 """
+
 TODO: (P1.1) Write code to make the robot drive in a square. Then,
 modify your code so that the robot drives in a square 3 times.
 
