cb2ec39a7e7281e0874297d289ce511a97cd9e4e
v <violetedwardsjane@gmail.com>
2024-09-16 14:04:24 -0400

    Update files

--- drive_square.py
+++ drive_square.py
@@ -10,28 +10,22 @@ def drive_square():
         robot.drive(0,0,1.57)
         time.sleep(1)
 
+def drive_square2():
+    robot.drive(1.0,0,0)
+    time.sleep(1)
+    robot.drive(0,1,0)
+    time.sleep(1)
+    robot.drive(-1,0,0)
+    time.sleep(1)
+    robot.drive(0,-1,0)
+    time.sleep(1)
+
 def drive_square_three_times():
     for i in range(3):
-        drive_square()
+        drive_square2()
 
-drive_square_three_times()
+try:
+    drive_square_three_times()
 
-"""
-
-TODO: (P1.1) Write code to make the robot drive in a square. Then,
-modify your code so that the robot drives in a square 3 times.
-
-HINT: A function to send velocity commands to the robot is provided. To
-use it, use the following code:
-
-    robot.drive(vx, vy, wz);
-
-Replace vx, vy, and wz with the velocity in the x direction (vx), y
-direction (vy), and the angular velocity (wz). You can also use this code:
-
-    time.sleep(secs);
-
-to sleep for "secs" seconds (replace with desired number of seconds).
-"""
-
-robot.stop()
+except:
+    robot.stop()
--- wall_follower.py
+++ wall_follower.py
@@ -1,8 +1,9 @@
 import time
 import numpy as np
 from mbot_bridge.api import MBot
+import math
 
-
+#Done
 def find_min_dist(ranges, thetas):
     """Finds the length and angle of the minimum ray in the scan.
 
@@ -15,10 +16,21 @@ def find_min_dist(ranges, thetas):
     Returns:
         tuple: The length and angle of the shortest ray in the Lidar scan.
     """
+    valid_rays = []
+    for dist, angle in zip(ranges, thetas):
+        if dist > 0:
+            valid_rays.append((dist, angle))
+
+    if not valid_rays:
+        return None, None
+    
+    valid_distances, valid_angles = [ray[0] for ray in valid_rays], [ray[1] for ray in valid_rays]
+
     min_dist, min_angle = None, None
-    min_dist = np.min(ranges)
-    index = ranges.index(min_dist)
-    min_angle = ranges[index]
+
+    min_dist = min(valid_distances)
+    index = valid_distances.index(min_dist)
+    min_angle = valid_angles[index]
 
     return min_dist, min_angle
 
@@ -41,6 +53,9 @@ def cross_product(v1, v2):
 robot = MBot()
 setpoint = 0  # TODO: Pick your setpoint.
 # TODO: Declare any other variables you might need here.
+ranges, thetas = robot.read_lidar()
+print(find_min_dist(ranges, thetas))
+
 
 try:
     while True:
@@ -50,8 +65,26 @@ try:
         # TODO: (P1.2) Write code to follow the nearest wall here.
         # Hint: You should use the functions cross_product and find_min_dist.
 
+        #Step 1 Find the distance to the nearest wall and the angle where the wall is located (use findMinDist() for this part).
+        min_dist,min_angle = find_min_dist(ranges,thetas)
+
+        #Step 2 Use the cross product to find a vector pointing parallel to the wall, in the direction the robot should drive.
+        v_to_wall = [1*math.cos(min_angle),1*math.sin(min_angle),0]
+
+        follow = cross_product(v_to_wall, [0,0,1])
+        print(follow)
+        
+
+        #Step 3 Apply a correction to the vector using bang-bang or P-control to move closer to or farther from the wall, depending on the current distance to the wall.
+    
+
+        #Step 4 Convert the vector to a velocity vector and send a velocity command to the robot.
+        robot.drive(follow[0], follow[1], follow[2])
+
+
+
         # Optionally, sleep for a bit before reading a new scan.
-        time.sleep(0.1)
+        time.sleep(0.5)
 except:
     # Catch any exception, including the user quitting, and stop the robot.
     robot.stop()
